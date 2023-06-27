facc49b63501413bfad1604772e0586b2ab1fae2
Yididiyah <yididiyazike@gmail.com>
2023-06-27 19:20:39 +0300

    show district info on button click works

--- src/components/Map/DynamicMap.js
+++ src/components/Map/DynamicMap.js
@@ -3,9 +3,10 @@ import * as ReactLeaflet from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 import styles from "@/styles/Map.module.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Typography } from "@mui/material";
 
-const { MapContainer, useMap } = ReactLeaflet;
+const { MapContainer, useMap, Popup } = ReactLeaflet;
 
 const Map = ({
   children,
@@ -22,22 +23,40 @@ const Map = ({
   }
   const ZoomToDistrict = () => {
     const map = useMap();
+    const [showPopup, setShowPopup] = useState(false);
+    const [popupLat, setPopupLat] = useState(0);
+    const [popupLng, setPopupLng] = useState(0);
+    const [name, setName] = useState("");
+    const [density, setDensity] = useState("");
     useEffect(() => {
       if (selectedDistrictFeature && map) {
         const geometry = Leaflet.geoJSON(selectedDistrictFeature.geometry);
-        //TODO: continue here to make district info visible on button click
-        const popup = Leaflet.popup()
-          .setLatLng([55.5, -1.9])
-          .setContent("<p>Test</p>")
-          .openOn(map);
+        const info = selectedDistrictFeature.properties;
+        const name = info.lvl1_name || info.lvl2_name;
+        setName(name);
+        const { density } = info;
+        setDensity(density);
+
         const geoJsonBounds = geometry.getBounds();
 
         map.fitBounds(geoJsonBounds);
+
         console.log("selectedDistrictFeature", selectedDistrictFeature);
-        console.log("map", map);
+        const { _northEast, _southWest } = geoJsonBounds;
+
+        // set Latitude and Longitude for the popup
+        setPopupLat((_northEast.lat + _southWest.lat) / 2);
+        setPopupLng((_northEast.lng + _southWest.lng) / 2);
+
+        setShowPopup(true);
       }
     }, [selectedDistrictFeature, map]);
-    return null;
+    return showPopup ? (
+      <Popup position={[popupLat, popupLng]}>
+        <Typography variant="h5">{name}</Typography>
+        <Typography variant="p">Density: {density}</Typography>
+      </Popup>
+    ) : null;
   };
 
   return (
